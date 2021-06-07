c6e26912fe2e10f00627e59f871e7ea8a50167d7
Tim Sullivan <timothyj.sullivan1@gmail.com>
2021-06-06 22:01:49 -0500

    small tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Welcome to the home of the Greater Minnesota Alumni Association</h1>
+    <h1>Welcome to the home of the Greater Minnesota Alumni Chapter</h1>
     <p>This site will host content about us, as well as applications built to support our operations.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/about/">About us</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
